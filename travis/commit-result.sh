c10ee6cd025f287518f1e7ada8bcc943d3805996
Antonio Cuni <anto.cuni@gmail.com>
2016-11-02 15:32:51 +0100

    try to run benchmarks on travis and commit/push results

---
+++ travis/commit-result.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+EMAIL=`git --no-pager show -s --format='<%ae>' HEAD`
+
+git remote set-url origin $SSH_REPO.git
+git config --global user.email "$EMAIL"
+git config --global user.name "Travis CI"
+
+chmod 600 ./travis/travis.rsa
+eval `ssh-agent -s`
+ssh-add ./travis/travis.rsa
+
+git add .benchmarks
+git commit -m "add benchmark results for commit $SHA"
+git push
