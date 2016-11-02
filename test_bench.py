c78244d4bb467d314cbbc7b41ad7ef7ab32a990b
Antonio Cuni <anto.cuni@gmail.com>
2016-11-02 11:59:34 +0100

    add some benchmarks

---
+++ test_bench.py
@@ -0,0 +1,13 @@
+from test import pystone
+
+def test_pystone(benchmark):
+    benchmark(pystone.main)
+
+
+def test_sum(benchmark):
+    def fn():
+        x = 0
+        for i in range(1000000):
+            x += i
+        return i
+    benchmark(fn)
