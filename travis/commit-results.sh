346e6aaa060e56de6403e59bb0bd0f65e6ea76a6
Antonio Cuni <anto.cuni@gmail.com>
2016-11-02 16:29:33 +0100

    another try

--- travis/commit-results.sh
+++ travis/commit-results.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 REPO=`git config remote.origin.url`
+TARGET_BRANCH=benchmarks
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 EMAIL=`git --no-pager show -s --format='<%ae>' HEAD`
 
-git remote set-url origin $SSH_REPO
+#git remote set-url origin $SSH_REPO
 git config --global push.default simple
 git config --global user.email "$EMAIL"
 git config --global user.name "Travis CI"
@@ -14,7 +15,7 @@ chmod 600 ./travis/travis.rsa
 eval `ssh-agent -s`
 ssh-add ./travis/travis.rsa
 
-git checkout benchmarks || git checkout --orphan benchmarks
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 git add .benchmarks
 git commit -m "add benchmark results for commit $SHA"
-git push
+git push $SSH_REPO $TARGET_BRANCH
