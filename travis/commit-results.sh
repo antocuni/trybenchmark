87f2e9afd1aacfb1c43c0b549748339a7bca16ad
Antonio Cuni <anto.cuni@gmail.com>
2016-11-03 11:47:51 +0100

    try another approach: clone the benchmarks branch *before* running the benchmarks

--- travis/commit-results.sh
+++ travis/commit-results.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-REPO=`git config remote.origin.url`
-TARGET_BRANCH=benchmarks
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+echo "Commit result..."
+set -v
+
+# get SHA and EMAIL of the current revision
 SHA=`git rev-parse --verify HEAD`
 EMAIL=`git --no-pager show -s --format='<%ae>' HEAD`
 
-#git remote set-url origin $SSH_REPO
-git config --global push.default simple
-git config --global user.email "$EMAIL"
-git config --global user.name "Travis CI"
+git config push.default simple
+git config user.email "$EMAIL"
+git config user.name "Travis CI"
 
+# setup ssh auth
 chmod 600 ./travis/travis.rsa
 eval `ssh-agent -s`
 ssh-add ./travis/travis.rsa
 
-git fetch origin $TARGET_BRANCH:$TARGET_BRANCH
-git checkout $TARGET_BRANCH || (git checkout --orphan $TARGET_BRANCH; git rm --cached -r .)
-git add .benchmarks
+# commit to benchrepo
+cd .benchmarks
+REPO=`git config remote.origin.url`
+TARGET_BRANCH=benchmarks
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+# git fetch origin $TARGET_BRANCH:$TARGET_BRANCH
+# git checkout $TARGET_BRANCH || (git checkout --orphan $TARGET_BRANCH; git rm --cached -r .)
+git add .
 git commit -m "add benchmark results for commit $SHA"
 git push $SSH_REPO $TARGET_BRANCH
