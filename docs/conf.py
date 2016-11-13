9bd61d34cc2d7ae5db5342bc48474cbd1d9bd53d
Travis CI <anto.cuni@gmail.com>
2016-11-13 19:14:25 +0100

    try to generate pygal charts in the doc

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,13 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+
+import os, sys
+sys.path.append(os.path.abspath('ext'))
+
+extensions = [
+    'pygal_sphinx_directives'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
