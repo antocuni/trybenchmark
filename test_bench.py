8db3757429ffaf2b067d37084053e6928a980b0e
Travis CI <anto.cuni@gmail.com>
2016-11-15 16:48:33 +0100

    try to measure how many CPUs we actually have

--- test_bench.py
+++ test_bench.py
@@ -2,12 +2,14 @@ import os
 import commands
 import time
 from test import pystone
+import multiprocessing
 
-def test_pystone(benchmark):
+
+def xtest_pystone(benchmark):
     benchmark(pystone.main)
 
 
-def test_sum(benchmark):
+def xtest_sum(benchmark):
     def fn():
         x = 0
         for i in range(1200000):
@@ -15,7 +17,7 @@ def test_sum(benchmark):
         return i
     benchmark(fn)
 
-def test_cpu():
+def xtest_cpu():
     import psutil
     p = psutil.Process()
     print
@@ -26,7 +28,7 @@ def test_cpu():
         usage = psutil.cpu_percent(0.1, percpu=True)
         print 'CPU', mycpu, usage[mycpu:mycpu+1]
 
-def test_cgroup():
+def xtest_cgroup():
     import py
     d = py.path.local('/sys/fs/cgroup/')
     for f in d.visit():
@@ -39,3 +41,27 @@ def test_cgroup():
         if '\n' in s:
             s = s.splitlines()[0] + '...'
         print f, s
+
+
+    
+def worker():
+    from test import pystone
+    a = time.time()
+    pystone.pystones()
+    b = time.time()
+    print '%.2f' % (b-a)
+
+def test_multiprocess():
+    def many(N):
+        jobs = []
+        for i in range(N):
+            p = multiprocessing.Process(target=worker)
+            jobs.append(p)
+            p.start()
+        for job in jobs:
+            job.join()
+
+    for N in (1, 2, 4, 8, 16, 32):
+        print 'N =', N
+        many(N)
+        print
