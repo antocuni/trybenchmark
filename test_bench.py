17516619b2c41e7e5e519441313af9448e05666f
Travis CI <anto.cuni@gmail.com>
2016-11-15 11:55:12 +0100

    .

--- test_bench.py
+++ test_bench.py
@@ -15,5 +15,9 @@ def test_sum(benchmark):
 
 def test_cpu():
     import psutil
-    for i in range(100):
+    for i in range(10):
         print 'CPU:', psutil.cpu_percent(0.1, percpu=True)
+
+def test_cgroup():
+    import os
+    os.system('cat /proc/%d/cgroup' % os.getpid())
