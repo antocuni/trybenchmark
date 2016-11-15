33e010f93bb29b6b0bacc143d055f5f6a4864e06
Travis CI <anto.cuni@gmail.com>
2016-11-15 12:12:08 +0100

    .

--- test_bench.py
+++ test_bench.py
@@ -1,3 +1,5 @@
+import os
+import commands
 import time
 from test import pystone
 
@@ -15,9 +17,11 @@ def test_sum(benchmark):
 
 def test_cpu():
     import psutil
+    p = psutil.Process()
+    print
+    print 'cpu affinity:', p.cpu_affinity()
     for i in range(10):
-        print 'CPU:', psutil.cpu_percent(0.1, percpu=True)
-
-def test_cgroup():
-    import os
-    os.system('cat /proc/%d/cgroup' % os.getpid())
+        mycpu = commands.getoutput('ps -o psr= -p %d' % os.getpid())
+        mycpu = int(mycpu)
+        usage = psutil.cpu_percent(0.1, percpu=True)
+        print 'CPU', mycpu, usage[mycpu:mycpu+1]
